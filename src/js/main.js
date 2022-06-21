8d9fdee87e6aa0375e284dc4a57007a9d190b2ad
Kamil Wróblewski <kamil.wroblewski.fd@gmail.com>
2022-06-21 13:26:02 +0200

    feat:JS fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,7 @@
 
 // service worker registration - remove if you're not going to use it
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+
 
 // place your code below
 
@@ -38,24 +28,25 @@ document.querySelector('.dateBox--JS').innerHTML = actualDay.toDateString();
 document.querySelector('.dateChecker--JS').innerHTML = `Actual date.`;
 addBottle.style.background = "#24c7e1";
 
-function Check(days){
+function Check(days) {
   document.querySelector('.dateBox--JS').innerHTML = actualDay.toDateString();
-if (days == 0){
-  document.querySelector('.dateChecker--JS').innerHTML = `Actual date.`;
-  addBottle.style.background = "#24c7e1";
-} else if (days == 1){
-  document.querySelector('.dateChecker--JS').innerHTML = `${days} day forward.`;
-  addBottle.style.background = "red";
-} else if (days >= 2){
-  document.querySelector('.dateChecker--JS').innerHTML = `${days} days forward.`;
-  addBottle.style.background = "red";
-} else if (days == -1) {
-  document.querySelector('.dateChecker--JS').innerHTML = `${Math.abs(days)} day ago.`;
-  addBottle.style.background = "red";
-} else if (days <= 0) {
-  document.querySelector('.dateChecker--JS').innerHTML = `${Math.abs(days)} days ago.`;
-  addBottle.style.background = "red";
-}};
+  if (days == 0) {
+    document.querySelector('.dateChecker--JS').innerHTML = `Actual date.`;
+    addBottle.style.background = "#24c7e1";
+  } else if (days == 1) {
+    document.querySelector('.dateChecker--JS').innerHTML = `${days} day forward.`;
+    addBottle.style.background = "red";
+  } else if (days >= 2) {
+    document.querySelector('.dateChecker--JS').innerHTML = `${days} days forward.`;
+    addBottle.style.background = "red";
+  } else if (days == -1) {
+    document.querySelector('.dateChecker--JS').innerHTML = `${Math.abs(days)} day ago.`;
+    addBottle.style.background = "red";
+  } else if (days <= 0) {
+    document.querySelector('.dateChecker--JS').innerHTML = `${Math.abs(days)} days ago.`;
+    addBottle.style.background = "red";
+  }
+};
 
 
 
@@ -67,11 +58,11 @@ const fill4 = document.querySelector('.fill__4');
 const fill5 = document.querySelector('.fill__5');
 const fill6 = document.querySelector('.fill__6');
 
-function counter(bottle){
-  if (bottle > 0){
-  document.querySelector('.counter--JS').innerHTML = bottle;
-  } else{ 
-  document.querySelector('.counter--JS').innerHTML = '';
+function counter(bottle) {
+  if (bottle > 0) {
+    document.querySelector('.counter--JS').innerHTML = bottle;
+  } else {
+    document.querySelector('.counter--JS').innerHTML = '';
   }
 }
 
@@ -202,26 +193,26 @@ removeBottle.addEventListener('click', function () {
 
 
 
-// switch (bottle) {
-//   case 0:
-//     console.log('nie piłeś dziś wody');
-//     break;
-//   case 1:
-//     console.log(`wypiłeś dziś ${bottle} butelke wody!`);
-//   break;
-//   case 2:
-//     console.log(`wypiłeś dziś ${bottle} butelki wody!`);
-//   break;
-//   case 3:
-//     console.log(`wypiłeś dziś ${bottle} butelki wody!`);
-//   break;
-//   case 4:
-//     console.log(`wypiłeś dziś ${bottle} butelki wody!`);
-//   break;
-//   case 5:
-//     console.log(`wypiłeś dziś ${bottle} butelek wody!`);
-//   break;
-//   case 6:
-//     console.log(`wypiłeś dziś ${bottle} butelek wody!`);
-//   break;
-// }
+switch (bottle) {
+  case 0:
+    console.log('nie piłeś dziś wody');
+    break;
+  case 1:
+    console.log(`wypiłeś dziś ${bottle} butelke wody!`);
+  break;
+  case 2:
+    console.log(`wypiłeś dziś ${bottle} butelki wody!`);
+  break;
+  case 3:
+    console.log(`wypiłeś dziś ${bottle} butelki wody!`);
+  break;
+  case 4:
+    console.log(`wypiłeś dziś ${bottle} butelki wody!`);
+  break;
+  case 5:
+    console.log(`wypiłeś dziś ${bottle} butelek wody!`);
+  break;
+  case 6:
+    console.log(`wypiłeś dziś ${bottle} butelek wody!`);
+  break;
+}
